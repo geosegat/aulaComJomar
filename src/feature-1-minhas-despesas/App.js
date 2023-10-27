6dd247138ae8309e59bedbb4d91f2bd66ec730f1
Geovani <geosegat@gmail.com>
2023-10-27 00:31:23 -0300

    feature salva

--- src/feature-1-minhas-despesas/App.js
+++ src/feature-1-minhas-despesas/App.js
@@ -7,44 +7,49 @@ import {
   Image,
 } from 'react-native';
 import React from 'react';
+import SvgVoltar from './svgs/seta-esquerda.svg';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#09498F" />
       <View style={styles.container2}>
-        <View style={styles.container4}>
-          <Image
-            source={{
-              uri: 'https://cdn-icons-png.flaticon.com/512/657/657001.png',
-            }}
-            style={styles.image}
-          />
-          <Text style={styles.fontVoltar}>Voltar</Text>
-        </View>
-
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            left: 15,
+            top: 15,
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <View style={{marginRight: 5}}>
+            <SvgVoltar width={18} height={18} color="white"></SvgVoltar>
+          </View>
+          <Text style={{color: 'white', fontSize: 17}}>Voltar</Text>
+        </TouchableOpacity>
         <View style={styles.container3}>
           <Text style={styles.fontMinhasDespesas}>Minhas despesas</Text>
           <Text style={styles.fontEtapa}>Etapa 1 de 3</Text>
         </View>
       </View>
 
-      <View style={styles.containerDespesa}>
+      <TouchableOpacity style={styles.containerDespesa}>
         <Text style={styles.fontAddDespesa}>+ ADICIONAR DESPESA</Text>
-      </View>
+      </TouchableOpacity>
       <Text style={styles.fontSize14}>
         Você pode adicionar quantas despesas quiser num mesmo pedido.
       </Text>
-      <Text style={styles.fontSize18}>Nenhuma despesa incluída</Text>
-      <Text style={styles.fontSize14}>
-        Toque em ADICIONAR DESPESA para incluir quantos procedimentos quiser.
-        Quando terminar de incluir toque em PRÓXIMO
-      </Text>
-      <View style={styles.containerButtom}>
-        <TouchableOpacity>
-          <Text style={styles.textProximo}>PRÓXIMO</Text>
-        </TouchableOpacity>
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text style={styles.fontSize18}>Nenhuma despesa incluída</Text>
+        <Text style={styles.fontSize14}>
+          Toque em ADICIONAR DESPESA para incluir quantos procedimentos quiser.
+          Quando terminar de incluir toque em PRÓXIMO
+        </Text>
       </View>
+
+      <TouchableOpacity style={styles.containerButtom}>
+        <Text style={styles.textProximo}>PRÓXIMO</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -56,17 +61,17 @@ const styles = StyleSheet.create({
   container2: {
     backgroundColor: '#09498F',
     flexDirection: 'row',
+    justifyContent: 'center',
+    paddingVertical: 5,
   },
   container3: {
     alignItems: 'center',
-    marginLeft: 64,
   },
   fontMinhasDespesas: {color: '#FFFFFF', fontSize: 17, fontWeight: 'bold'},
-  fontEtapa: {color: '#FFFFFF', fontSize: 12, marginBottom: 10},
+  fontEtapa: {color: '#FFFFFF', fontSize: 12},
   container4: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 12,
   },
   image: {width: 20, height: 25},
   fontVoltar: {color: '#FFFFFF', fontSize: 18},
@@ -89,24 +94,21 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#555555',
     textAlign: 'center',
-    margin: 15,
-    marginTop: 10,
   },
   fontSize18: {
     fontSize: 18,
     color: '#555555',
     textAlign: 'center',
     margin: 15,
-    marginTop: 139,
     fontWeight: 'bold',
     marginBottom: 20,
   },
   containerButtom: {
-    marginTop: 140,
-    flex: 1,
     backgroundColor: '#09498F',
     alignItems: 'center',
     justifyContent: 'center',
+    opacity: 1,
+    paddingVertical: 25,
   },
   textProximo: {fontSize: 16, color: '#FFFFFF', fontWeight: 'bold'},
 });
