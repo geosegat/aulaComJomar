763380a5a60f4f847e839128a3814ca5944a5822
Geovani <geosegat@gmail.com>
2023-10-27 01:12:48 -0300

    feature-reembolso adicionado status bar

--- src/feature-2-reembolso/App.js
+++ src/feature-2-reembolso/App.js
@@ -1,16 +1,17 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import SvgVoltar from './svgs/seta-esquerda.svg';
 
 const App = () => {
   return (
-    <View style={{backgroundColor: 'pink'}}>
-      <SvgVoltar width={18} height={18} color="white"></SvgVoltar>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <StatusBar></StatusBar>
+      <Text>aa</Text>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'purple'},
+});
