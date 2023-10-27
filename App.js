0b17471c3c18427d2313b7882e5b115bacb7c945
Geovani <geosegat@gmail.com>
2023-10-27 00:40:48 -0300

    develop

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import SvgVoltar from './svgs/seta-esquerda.svg';
 
 const App = () => {
   return (
-    <View style={{backgroundColor: 'pink'}}>
-      <SvgVoltar width={18} height={18} color="white"></SvgVoltar>
+    <View>
       <Text>App</Text>
     </View>
   );
