fbd2266d4590b5c22f5f376feb0e365f7ec94a95
Geovani <geosegat@gmail.com>
2023-10-30 15:53:19 -0300

    feature-page-64 finalizada

--- src/feature-page-64/App.js
+++ src/feature-page-64/App.js
@@ -20,10 +20,53 @@ const App = () => {
         <Text style={styles.fontDadosBancarios}>Dados Bancários</Text>
         <Text style={styles.fontEtapasText}>Etapa 2 de 3</Text>
       </View>
-      <View style={{flex: 1, backgroundColor: 'white', padding: 15}}>
+      <View style={styles.container2}>
         <TouchableOpacity style={styles.subContainer}>
           <Text style={styles.fontAddNovaConta}>+ ADICIONAR NOVA CONTA</Text>
         </TouchableOpacity>
+        <View style={styles.containerTextContadasCadastradas}>
+          <Text style={styles.fontContasCadastradas}>Contas cadastradas</Text>
+        </View>
+        <View style={styles.containerDadosCadastrados}>
+          <View style={styles.containerTextDadosCadastrados}>
+            <View style={styles.fontMarginBot2}>
+              <Text style={styles.fontBancoNome}>Banco do Brasil SA</Text>
+            </View>
+            <View style={styles.fontMarginBot2}>
+              <Text style={styles.fontInformacoesDasContas}>
+                Victor Hugo Prando da Silva
+              </Text>
+            </View>
+            <View style={styles.containerAgCc}>
+              <Text style={styles.fontInformacoesDasContas}>Ag.: 0070</Text>
+              <View style={styles.containerEspaco}></View>
+              <Text style={styles.fontInformacoesDasContas}>CC: 12099-0</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.containerSetaIr}>
+            <SvgSeta color="#09498F" width={15} height={15} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerDadosCadastrados}>
+          <View style={styles.containerTextDadosCadastrados}>
+            <View style={styles.fontMarginBot2}>
+              <Text style={styles.fontBancoNome}>Banco do Brasil SA</Text>
+            </View>
+            <View style={styles.fontMarginBot2}>
+              <Text style={styles.fontInformacoesDasContas}>
+                Victor Hugo Prando da Silva
+              </Text>
+            </View>
+            <View style={styles.containerAgCc}>
+              <Text style={styles.fontInformacoesDasContas}>Ag.: 0070</Text>
+              <View style={styles.containerEspaco}></View>
+              <Text style={styles.fontInformacoesDasContas}>CC: 12099-0</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.containerSetaIr}>
+            <SvgSeta color="#09498F" width={15} height={15} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -49,6 +92,7 @@ const styles = StyleSheet.create({
   fontButtonVoltar: {fontSize: 17, color: '#FFF'},
   fontDadosBancarios: {fontSize: 17, color: '#FFF', fontWeight: '500'},
   fontEtapasText: {fontSize: 12, color: '#FFF'},
+  container2: {flex: 1, backgroundColor: 'white', padding: 15},
   subContainer: {
     borderWidth: 1,
     borderStyle: 'dashed',
@@ -56,6 +100,24 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignItems: 'center',
     padding: 15,
+    marginBottom: 20,
   },
   fontAddNovaConta: {fontSize: 14, fontWeight: '500', color: '#09498F'},
+  containerTextContadasCadastradas: {marginBottom: 10},
+  fontContasCadastradas: {fontSize: 20, color: '#333', fontWeight: '500'},
+  containerDadosCadastrados: {
+    backgroundColor: '#edf0f6',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 5,
+    padding: 15,
+    marginBottom: 10,
+  },
+  containerTextDadosCadastrados: {flex: 1},
+  fontMarginBot2: {marginBottom: 3},
+  fontBancoNome: {color: '#333', fontWeight: '500'},
+  fontInformacoesDasContas: {fontSize: 12, color: '#555'},
+  containerAgCc: {flexDirection: 'row'},
+  containerEspaco: {marginHorizontal: 15},
+  containerSetaIr: {transform: [{rotateZ: '180deg'}]},
 });
