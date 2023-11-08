729172afc60de6c05262877d394ef904709c024d
Geovani <geosegat@gmail.com>
2023-11-08 18:58:49 -0300

    screen-74

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Screen64 from './src/screens/screen-64';
+import Screen74 from './src/screens/screen-74';
 
 const App = () => {
   return (
     <View>
-      <Screen64></Screen64>
+      <Screen74></Screen74>
     </View>
   );
 };
