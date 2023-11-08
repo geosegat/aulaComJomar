1cfd5b7f63e720fa70aef4925cec5177d9b2f494
Geovani <geosegat@gmail.com>
2023-11-08 15:35:52 -0300

    a

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import BlueCard from './src/components/BlueCard';
-import AppText from './src/components/AppText';
-import Navbar from './src/components/Navbar';
-import Screen64 from './src/screens/screen-64';
 
 const App = () => {
   return (
     <View>
-      <Screen64 />
+      <Text>App</Text>
     </View>
   );
 };
