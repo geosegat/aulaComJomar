a074c0cbf4a5156dbc228169e31ec7d1ac2b6f44
Geovani <geosegat@gmail.com>
2023-11-15 01:12:44 -0300

    bankAccountCard componente finalizado

--- src/refundComponents/BankAccountCard/index.js
+++ src/refundComponents/BankAccountCard/index.js
@@ -1,7 +1,8 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
 import AppText from '../../components/AppText';
 import App from '../../../App';
+import SvgSeta from './../../../svgs/seta-esquerda.svg';
 
 const BankAccountCard = ({
   bankName,
@@ -9,27 +10,33 @@ const BankAccountCard = ({
   bankBranch,
   bankAccount,
   style,
+  showSetaIcon = false,
 }) => {
   return (
     <View style={[styles.containerCard, style]}>
-      <View>
+      <View style={styles.containerTexts}>
         <AppText variant="semiBold">{bankName ?? 'Banco do Brasil SA'}</AppText>
         <View style={styles.containerBankNameMargin}>
           <AppText size="small" color="#555">
             {userName ?? 'Victor Hugo Prando da Silva'}
           </AppText>
         </View>
-      </View>
-      <View style={styles.containerBankBranchAccount}>
-        <AppText size="small" color="#555">
-          {bankBranch ?? 'Ag.: 0070'}
-        </AppText>
-        <View style={styles.containerBankAccount}>
+        <View style={styles.containerBankBranchAccount}>
           <AppText size="small" color="#555">
-            {bankAccount ?? 'CC: 12099-0'}
+            {bankBranch ?? 'Ag.: 0070'}
           </AppText>
+          <View style={styles.containerBankAccount}>
+            <AppText size="small" color="#555">
+              {bankAccount ?? 'CC: 12099-0'}
+            </AppText>
+          </View>
         </View>
       </View>
+      {showSetaIcon && (
+        <TouchableOpacity style={styles.containerSetaIr}>
+          <SvgSeta width={12} height={12} color={'#09498F'} />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
@@ -37,8 +44,18 @@ const BankAccountCard = ({
 export default BankAccountCard;
 
 const styles = StyleSheet.create({
-  containerCard: {padding: 15, elevation: 2, backgroundColor: 'white'},
+  containerCard: {
+    padding: 15,
+    elevation: 2,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  containerTexts: {flex: 1},
   containerBankNameMargin: {marginTop: 2},
   containerBankBranchAccount: {flexDirection: 'row', marginTop: 2},
   containerBankAccount: {marginLeft: 25},
+  containerSetaIr: {
+    transform: [{rotateZ: '180deg'}],
+  },
 });
