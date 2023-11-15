7ba89c0b59e260f8722621779417321b5bf7297e
Geovani <geosegat@gmail.com>
2023-11-15 05:10:46 -0300

    app padrao

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import RefundCard from './src/refundComponents/RefundCard';
-import BankAccountCard from './src/refundComponents/BankAccountCard';
 
 const App = () => {
-  function pressEditIcon() {
-    console.log('Botão de editar pressionado!');
-  }
-
-  function pressDeleteIcon() {
-    console.log('Botão de fechar pressionado!');
-  }
   return (
-    <View style={{flex: 1, backgroundColor: '#F6F6F6', padding: 15}}>
-      <View>
-        <BankAccountCard></BankAccountCard>
-      </View>
+    <View>
+      <Text>App</Text>
     </View>
   );
 };
