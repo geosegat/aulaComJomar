10b0a5a66a75c8af6878ba359c38328a1bfa7ad1
Geovani <geosegat@gmail.com>
2023-11-19 21:49:38 -0300

    bank

--- src/screens/Screen81/index.js
+++ src/screens/Screen81/index.js
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from '../../components/Navbar';
 import ContrastLabel from '../../refundComponents/ContrastLabel';
 import RadioButtonSelect from '../../refundComponents/RadioButtonSelect';
@@ -9,53 +9,81 @@ import {useNavigation} from '@react-navigation/native';
 const Screen81 = () => {
   const navigation = useNavigation();
 
+  const [isSelected, setIsSelected] = useState(false);
+
+  const onPress = () => {
+    setIsSelected(prev => !prev);
+    setIsSelected2(false);
+  };
+
+  const [isSelected2, setIsSelected2] = useState(false);
+
+  const onPress2 = () => {
+    setIsSelected2(prev => !prev);
+  };
+
   const goToScreen80 = () => {
     navigation.navigate('Screen80');
   };
 
   return (
-    <View style={styles.container}>
-      <Navbar onPressBotaoBack={goToScreen80} label={'Adicionar despesa'} />
-      <ContrastLabel
-        style={styles.containerContrastLabel}
-        label={'Itens autorizados com saldo disponível'}
-      />
-      <View style={styles.containerItems}>
-        <RadioButtonSelect
-          disabled
-          style={styles.marginTop}
-          label={'Atorvastatina Cálcica'}
+    <View style={{flex: 1}}>
+      <View style={styles.container}>
+        <Navbar onPressBotaoBack={goToScreen80} label={'Adicionar despesa'} />
+        <ContrastLabel
+          style={styles.containerContrastLabel}
+          label={'Itens autorizados com saldo disponível'}
         />
-        <RadioButtonSelect
-          disabled
-          style={styles.marginTop}
-          label={'Clortalidona'}
-        />
-        <RadioButtonSelect
-          disabled
-          style={styles.marginTop}
-          label={'Janumet'}
-        />
-      </View>
-      <ContrastLabel label={'Apresentação'} />
-      <View style={styles.containerRadioButton}>
-        <RadioButtonSelect
-          disabled
-          style={styles.marginTop}
-          label={'Atorvastatina Cálcica'}
-        />
-        <RadioButtonSelect
-          disabled
-          style={styles.marginTop}
-          label={'Clortalidona'}
-        />
-        <RadioButtonSelect
-          disabled
-          style={styles.marginTop}
-          label={'Janumet'}
-        />
-      </View>
+        <View style={styles.containerItems}>
+          <RadioButtonSelect
+            style={styles.radioButton}
+            label={'Atorvastatina Cálcica'}
+            onPressText={onPress}
+            isSelected={isSelected}
+          />
+          <RadioButtonSelect
+            style={styles.radioButton}
+            label={'Clortalidona'}
+          />
+          <RadioButtonSelect label={'Janumet'} />
+        </View>
 
+        {isSelected && (
+          <View>
+            <ContrastLabel label={'Apresentação'} />
+            <View style={styles.containerItems}>
+              <RadioButtonSelect
+                onPressText={onPress2}
+                isSelected={isSelected2}
+                style={styles.radioButton}
+                label={'Atorvastatina Cálcica'}
+              />
+              <RadioButtonSelect
+                style={styles.radioButton}
+                label={'Clortalidona'}
+              />
+              <RadioButtonSelect label={'Janumet'} />
+            </View>
+          </View>
+        )}
+
+        {isSelected2 && (
+          <View>
+            <ContrastLabel label={'Apresentação'} />
+            <View style={styles.containerItems}>
+              <RadioButtonSelect
+                style={styles.radioButton}
+                label={'Atorvastatina Cálcica'}
+              />
+              <RadioButtonSelect
+                style={styles.radioButton}
+                label={'Clortalidona'}
+              />
+              <RadioButtonSelect label={'Janumet'} />
+            </View>
+          </View>
+        )}
+      </View>
       <FooterButton label={'CONTINUAR'} showHideButtonOff />
     </View>
   );
@@ -65,9 +93,8 @@ export default Screen81;
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: 'rgba(255, 255, 255, 1)'},
-  containerItems: {marginHorizontal: 17, flex: 1},
-  containerRadioButton: {marginHorizontal: 17, flex: 2},
-
-  marginTop: {marginTop: 25},
+  containerItems: {padding: 15},
+  containerRadioButton: {marginHorizontal: 17},
   containerContrastLabel: {marginTop: 10},
+  radioButton: {marginBottom: 10},
 });
