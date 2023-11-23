d14aed6fe270ff9706fd40828f6f01f88ab5293c
Geovani <geosegat@gmail.com>
2023-11-23 19:13:27 -0300

    app em construção

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TelaInicio from './src/screens/AulaComJomar/TelaInicio';
-import Contador from './src/screens/AulaComJomar/Contador';
+import Contador from './src/screens/AulaComJomar/Counter';
+import TaskScreen from './src/screens/AulaComJomar/TaskScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,7 @@ const App = () => {
         screenOptions={{headerStyle: {backgroundColor: '#547789'}}}>
         <Stack.Screen name="Inicio" component={TelaInicio} />
         <Stack.Screen name="Contador" component={Contador} />
+        <Stack.Screen name="TaskScreen" component={TaskScreen}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
