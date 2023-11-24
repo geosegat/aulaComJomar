fca43f5f8747618366b78635bf6a7e3ac775cfcf
Geovani <geosegat@gmail.com>
2023-11-24 00:16:06 -0300

    meu app

--- src/screens/AulaComJomar/TelaInicio/copiar.js
+++ src/screens/AulaComJomar/TelaInicio/copiar.js
@@ -2,27 +2,51 @@ import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import React, {useState} from 'react';
 
 const Screen = () => {
-  const [listaDeTextos, setListaDeTextos] = useState([]);
-  const [textRecebido, setTextRecebido] = useState('');
-  const onChangeText = e => {
-    setTextRecebido(e);
+  const [listaTextos, setListaTextos] = useState > [];
+  const [input, setInput] = useState('');
+  const [inputDelete, setInputDelete] = useState('');
+
+  const onPress = () => {
+    if (input === '') {
+      return;
+    }
+    setListaTextos(prev => [...prev, input]);
+    setInput('');
   };
-  console.log(listaDeTextos);
+
+  const onChangeText = e => {
+    setInput(e);
+  };
+
+  const onPressDeleteText = () => {
+    setListaTextos(prev => {
+      return prev.filter(i => i.toLowerCase() !== inputDelete.toLowerCase());
+    });
+  };
+
+  const onChangeTextDelete = e => {
+    setInputDelete(e);
+  };
+
   return (
-    <View style={styles.container}>
-      {listaDeTextos.map(e => (
-        <Text key={Math.random()}>{e}</Text>
+    <View>
+      {listaTextos.map(i => (
+        <Text style={styles.text} key={Math.random()}>
+          {i}
+        </Text>
       ))}
       <TextInput
+        value={input}
         onChangeText={onChangeText}
-        style={styles.containerTextInput}
-        placeholder="digita aqui"
-        placeholderTextColor={'rgba(0,0,0,0.65)'}></TextInput>
-      <Button
-        title="me aperte"
-        onPress={() => {
-          setListaDeTextos(prev => [...prev, textRecebido]);
-        }}></Button>
+        style={styles.input}
+      />
+      <Button title="adicionar texto" onPress={onPress} />
+      <TextInput
+        value={inputDelete}
+        onChangeText={onChangeTextDelete}
+        style={styles.input}
+      />
+      <Button title="deletar texto" onPress={onPressDeleteText} />
     </View>
   );
 };
@@ -30,14 +54,6 @@ const Screen = () => {
 export default Screen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.08)',
-    justifyContent: 'center',
-  },
-  containerTextInput: {
-    padding: 15,
-    borderRadius: 8,
-    backgroundColor: 'white',
-  },
+  input: {backgroundColor: 'rgba(0,255,0,0.4)'},
+  text: {fontSize: 18, fontWeight: '500'},
 });
