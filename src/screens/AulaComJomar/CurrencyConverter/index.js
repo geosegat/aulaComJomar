d0e53d0cb2879d750d5350b27c323f14bcb421a2
Geovani <geosegat@gmail.com>
2023-12-01 20:42:20 -0300

    ok

--- src/screens/AulaComJomar/CurrencyConverter/index.js
+++ src/screens/AulaComJomar/CurrencyConverter/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   StyleSheet,
@@ -6,100 +6,190 @@ import {
   TouchableOpacity,
   TextInput,
   Text,
+  FlatList,
 } from 'react-native';
+
+const currencies = [
+  {
+    moeda: 'Ark',
+    moedaP: 'Arks',
+    valor: 5.2,
+    image: 'https://dolarhoje.com/img/icon-ark.png',
+  },
+  {
+    moeda: 'Binancecoin',
+    moedaP: 'Binancecoins',
+    valor: 1119.34,
+    image: 'https://dolarhoje.com/img/icon-binance-coin.png',
+  },
+  {
+    moeda: 'Iota',
+    moedaP: 'Iotas',
+    valor: 1.12,
+    image: 'https://dolarhoje.com/img/icon-iota.png',
+  },
+  {
+    moeda: 'Litecoin',
+    moedaP: 'Litecoins',
+    valor: 343.93,
+    image: 'https://dolarhoje.com/img/icon-litecoin.gif',
+  },
+  {
+    moeda: 'Xem',
+    moedaP: 'Xems',
+    valor: 0.17,
+    image: 'https://dolarhoje.com/img/icon-nem.png',
+  },
+  {
+    moeda: 'Xrp',
+    moedaP: 'Xrps',
+    valor: 3,
+    image: 'https://dolarhoje.com/img/icon-xrp.png',
+  },
+  {
+    moeda: 'Salt',
+    moedaP: 'Salts',
+    valor: 0.18,
+    image: 'https://dolarhoje.com/img/icon-salt.png',
+  },
+  {
+    moeda: 'Shiba',
+    moedaP: 'Shibas',
+    valor: 0.000041,
+    image: 'https://dolarhoje.com/img/icon-shiba-inu.png',
+  },
+  {
+    moeda: 'Tron',
+    moedaP: 'Trons',
+    valor: 0.5,
+    image: 'https://dolarhoje.com/img/icon-tron.png',
+  },
+  {
+    moeda: 'Bitcoin',
+    moedaP: 'Bitcoins',
+    valor: 186203,
+    image: 'https://dolarhoje.com/img/icon-bitcoin.gif',
+  },
+  {
+    moeda: 'Dolar',
+    moedaP: 'Dolares',
+    valor: 4.9,
+    image: 'https://dolarhoje.com/img/icon-dolar.gif',
+  },
+  {
+    moeda: 'Euro',
+    moedaP: 'Euros',
+    valor: 5.36,
+    image: 'https://dolarhoje.com/img/icon-euro.gif',
+  },
+  {
+    moeda: 'Libra',
+    moedaP: 'Libras',
+    valor: 6.17,
+    image: 'https://dolarhoje.com/img/icon-libra.gif',
+  },
+  {
+    moeda: 'Dogecoin',
+    moedaP: 'Dogecoins',
+    valor: 0.38,
+    image: 'https://dolarhoje.com/img/icon-dogecoin.png',
+  },
+];
 
 const CurrencyConverter = () => {
   const [text, setText] = useState('');
   const [selectedCurrency, setSelectedCurrency] = useState(null);
   const [result, setResult] = useState(null);
+  const [filteredCurrencies, setFilteredCurrencies] = useState([]);
+  const [resultValue, setResultValue] = useState('');
 
   const handleTextChange = newText => {
-    const filteredText = newText.replace(/[^0-9.]/g, '');
-    setText(filteredText);
+    setText(newText);
+
+    const filtered = currencies.filter(currency =>
+      currency.moeda.toLowerCase().includes(newText.toLowerCase()),
+    );
+    setFilteredCurrencies(filtered);
   };
 
   const handleCurrencyPress = currency => {
     setSelectedCurrency(currency);
-    calculateResult();
   };
 
   const calculateResult = () => {
     if (text !== '' && selectedCurrency !== null) {
       const calculatedResult = parseFloat(text) * selectedCurrency.valor;
-      setResult({
-        value: calculatedResult.toFixed(2),
+      setResult(prevResult => ({
+        value: calculatedResult.toFixed(5),
         currencySing: selectedCurrency.moeda,
         currencyPlu: selectedCurrency.moedaP,
-      });
-    }
-  };
-  const getResultMessage = () => {
-    if (text > 1 && result) {
-      return ` ${text} ${result.currencyPlu} valem R$ ${result.value}`;
-    } else if (text == 1 && result) {
-      return ` ${text} ${result.currencySing} vale R$ ${result.value}`;
+      }));
+      setResultValue(calculatedResult.toFixed(5));
     } else {
-      return 'Digite um valor e selecione a moeda desejada.';
+      setResult(null);
+      setResultValue('');
     }
   };
 
-  const currencies = [
-    {
-      moeda: 'Bitcoin',
-      moedaP: 'Bitcoins',
-      valor: 186203,
-      image: 'https://dolarhoje.com/img/icon-bitcoin.gif',
-    },
-    {
-      moeda: 'Dolar',
-      moedaP: 'Dolares',
-      valor: 4.9,
-      image: 'https://dolarhoje.com/img/icon-dolar.gif',
-    },
-    {
-      moeda: 'Euro',
-      moedaP: 'Euros',
-      valor: 5.36,
-      image: 'https://dolarhoje.com/img/icon-euro.gif',
-    },
-    {
-      moeda: 'Libra',
-      moedaP: 'Libras',
-      valor: 6.17,
-      image: 'https://dolarhoje.com/img/icon-libra.gif',
-    },
-    {
-      moeda: 'Dogecoin',
-      moedaP: 'Dogecoins',
-      valor: 0.38,
-      image: 'https://dolarhoje.com/img/icon-dogecoin.png',
-    },
-  ];
+  useEffect(() => {
+    calculateResult();
+  }, [text, selectedCurrency]);
+
+  const handleResultChange = newValue => {
+    setResultValue(newValue);
+  };
+
+  const getResultMessage = () => {
+    if (result !== null && text !== '') {
+      if (text > 1) {
+        return `${text} ${result.currencyPlu} valem R$ ${result.value}`;
+      } else if (text == 1) {
+        return `${text} ${result.currencySing} vale R$ ${result.value}`;
+      }
+    }
+    return 'Digite um valor e selecione a moeda desejada.';
+  };
+
+  const renderItem = ({item}) => (
+    <TouchableOpacity
+      style={styles.marginImg}
+      onPress={() => {
+        console.log('fui');
+        handleCurrencyPress(item);
+      }}>
+      <Image width={45} height={45} source={{uri: item.image}} />
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
-      <View style={styles.containerImg}>
-        {currencies.map((currency, index) => (
-          <TouchableOpacity
-            style={styles.marginImg}
-            key={index}
-            onPress={() => {
-              handleCurrencyPress(currency);
-            }}>
-            <Image width={45} height={45} source={{uri: currency.image}} />
-          </TouchableOpacity>
-        ))}
+      <View>
+        <View style={styles.caixaHeight} />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Pesquisar moeda"
+          placeholderTextColor={'#fff'}
+          onChangeText={handleTextChange}
+          value={text}
+        />
+        <FlatList
+          data={filteredCurrencies.length > 0 ? filteredCurrencies : currencies}
+          renderItem={renderItem}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
       </View>
-      <TextInput
-        onChangeText={handleTextChange}
-        keyboardType="numeric"
-        style={styles.styleTextInput}
-        placeholder="0"
-        placeholderTextColor={'#E6E6E46f'}
-        value={text}
-        onBlur={calculateResult}
-      />
-      <Text style={styles.resultText}>{getResultMessage()}</Text>
+      <View style={styles.resultContainer}>
+        <TextInput
+          style={styles.resultText}
+          placeholder="0"
+          keyboardType="numeric"
+          placeholderTextColor="#E6E6E46f"
+          value={resultValue}
+          onChangeText={handleResultChange}
+        />
+        <Text style={styles.resultText}>{getResultMessage()}</Text>
+      </View>
     </View>
   );
 };
@@ -107,14 +197,30 @@ const CurrencyConverter = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     backgroundColor: '#2A2A2B',
     paddingHorizontal: 15,
   },
-  containerImg: {flexDirection: 'row', padding: 15},
-  styleTextInput: {color: '#fff', marginTop: 10, marginBottom: 10},
+  styleTextInput: {
+    color: '#fff',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  resultContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   resultText: {color: '#fff', marginTop: 10},
-  marginImg: {marginLeft: 12},
+  marginImg: {marginRight: 15},
+  caixaHeight: {height: 50},
+  searchInput: {
+    height: 50,
+    borderColor: '#547789',
+    borderWidth: 1,
+    color: '#fff',
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
 });
 
 export default CurrencyConverter;
