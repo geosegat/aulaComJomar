48bac18197b18f656cc0330e3659e9f0899e0a17
Geovani <geosegat@gmail.com>
2023-12-20 19:15:13 -0300

    att

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import TelaInicio from './src/screens/AulaComJomar/TelaInicio';
-import Contador from './src/screens/AulaComJomar/Counter';
-import TaskScreen from './src/screens/AulaComJomar/TaskScreen';
-import CurrencyConverter from './src/screens/AulaComJomar/CurrencyConverter';
-import AuthenticationField from './src/screens/AulaComJomar/AuthenticationField';
-import CepResultsScreen from './src/screens/AulaComJomar/CepResultsScreen';
-import VirtualCardGenerator from './src/screens/AulaComJomar/VirtualCardGenerator';
-import DDDetect from './src/screens/AulaComJomar/DDDetect';
+import InitialScreen from './src/Screens/InitialScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,24 +12,8 @@ const pressButon = () => {
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{headerStyle: {backgroundColor: '#547789'}}}>
-        <Stack.Screen name="Inicio" component={TelaInicio} />
-        <Stack.Screen name="Contador" component={Contador} />
-        <Stack.Screen name="TaskScreen" component={TaskScreen}></Stack.Screen>
-        <Stack.Screen
-          name="CurrencyConverter"
-          component={CurrencyConverter}></Stack.Screen>
-        <Stack.Screen
-          name="AuthenticationField"
-          component={AuthenticationField}></Stack.Screen>
-        <Stack.Screen
-          name="CepResultsScreen"
-          component={CepResultsScreen}></Stack.Screen>
-        <Stack.Screen
-          name="VirtualCardGenerator"
-          component={VirtualCardGenerator}></Stack.Screen>
-        <Stack.Screen name="DDDetect" component={DDDetect}></Stack.Screen>
+      <Stack.Navigator screenOptions={{headerStyle: {backgroundColor: 'gray'}}}>
+        <Stack.Screen name="InitialScreen" component={InitialScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
