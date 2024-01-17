4cddc76db1eb6e1689fba7a65398a617fc4d30f3
Geovani <geosegat@gmail.com>
2024-01-17 18:44:28 -0300

    finalizado

--- src/screens/AulaComJomar/Quiz/index.js
+++ src/screens/AulaComJomar/Quiz/index.js
@@ -1,4 +1,4 @@
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, TouchableOpacity, View} from 'react-native';
 import React, {useState} from 'react';
 import QuestButton from '../../../components/QuestButton';
 import AppText from '../../../components/AppText';
@@ -7,10 +7,11 @@ const questions = require('../../../json/Questions/quiz.json');
 
 const Quiz = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [numberQuestion, setNumberQuestion] = useState(0);
   const [validarResposta, setValidarResposta] = useState('');
   const [colorResposta, setColorResposta] = useState('white');
   const [buttonNext, setButtonNext] = useState(true);
-  const [buttonLabel, setButtonlabel] = useState('Confirmar resposta');
+  const [buttonLabel, setButtonlabel] = useState('Selecione uma resposta');
   const [answerIndex, setAnswerIndex] = useState(null);
   const [disabledButton, setDisabledButton] = useState(false);
   const [score, setScore] = useState({certo: 0, errado: 0});
@@ -20,35 +21,59 @@ const Quiz = () => {
     setButtonNext(false);
     setValidarResposta(`Resposta ${selectedOptionIndex + 1} selecionada.`);
     setDisabledButton(true);
+    setButtonlabel('Confirmar resposta');
   };
 
   const handleNextQuestion = () => {
     if (buttonLabel.includes('Confirmar')) {
       if (answerIndex === questions.questions[currentQuestion].correctIndex) {
         setValidarResposta('Resposta correta');
-        setButtonlabel('Proxima pergunta.');
+        setButtonlabel('Proxima pergunta');
         setScore({certo: score.certo + 1, errado: score.errado});
+        setColorResposta('green');
       } else {
         setValidarResposta('Resposta incorreta');
-        setButtonlabel('Proxima pergunta.');
+        setButtonlabel('Proxima pergunta');
         setScore({certo: score.certo, errado: score.errado + 1});
+        setColorResposta('red');
       }
     } else {
       setCurrentQuestion(currentQuestion + 1);
       setValidarResposta('');
-      setButtonlabel('Confirmar resposta');
+      setButtonlabel('Selecione uma resposta');
       setButtonNext(true);
       setDisabledButton(false);
+      setColorResposta('white');
     }
+  };
+
+  const onPressReset = () => {
+    setScore({certo: 0, errado: 0});
+    setCurrentQuestion(0);
   };
 
   console.log(currentQuestion);
 
   if (questions.questions.length === currentQuestion) {
     return (
-      <View>
-        <AppText>Você acertou: {score.certo}</AppText>
-        <AppText>Você errou: {score.errado}</AppText>
+      <View style={styles.container}>
+        <View
+          style={{
+            alignItems: 'center',
+            flex: 1,
+            justifyContent: 'center',
+          }}>
+          <AppText style={styles.respostasContainer} size="huge" color="white">
+            Sua pontuação final foi:
+          </AppText>
+          <AppText size="huge" color="green">
+            {score.certo} Respostas corretas
+          </AppText>
+          <AppText style={styles.respostasContainer} size="huge" color="red">
+            {score.errado} Respostas incorretas
+          </AppText>
+          <QuestButton onPress={onPressReset} label={'Refaça o quiz'} />
+        </View>
       </View>
     );
   }
@@ -56,8 +81,8 @@ const Quiz = () => {
   return (
     <View style={styles.container}>
       <View style={styles.containerCenterText}>
-        <AppText size="huge" color="white">
-          Pergunta n° {currentQuestion + 1}
+        <AppText style={styles.respostasContainer} size="huge" color="white">
+          Pergunta n° {numberQuestion + 1}
         </AppText>
         <AppText size="huge" color="white">
           {questions.questions[currentQuestion]?.question}
@@ -87,6 +112,7 @@ const Quiz = () => {
         label={buttonLabel}
         onPress={handleNextQuestion}
       />
+      <View style={{alignItems: 'center', marginTop: 25}}></View>
     </View>
   );
 };
@@ -99,6 +125,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#2A2A2B',
     padding: 25,
   },
-  containerCenterText: {alignItems: 'center'},
-  respostasContainer: {marginBottom: 15},
+  containerCenterText: {alignItems: 'center', marginBottom: 20},
+  respostasContainer: {marginBottom: 20},
 });
